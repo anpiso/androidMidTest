fb91ecdbac5a8952f13ec69b85be59176cd72361
Anpiso <ehdwnd9238@gmail.com>
2020-05-06 17:10:34 +0900

    중간고사 2

--- app/src/main/java/net/skhu/androidmidtest/Exam3Activity.java
+++ app/src/main/java/net/skhu/androidmidtest/Exam3Activity.java
@@ -1,15 +1,38 @@
 package net.skhu.androidmidtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class Exam3Activity extends AppCompatActivity {
+    exam3Adapter exam3Adapter;
+    ArrayList<String> arrayList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam3);
+
+        arrayList = new ArrayList<String>();
+        arrayList.add("one");
+        arrayList.add("two");
+
+        exam3Adapter = new exam3Adapter(this, arrayList);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(exam3Adapter);
+
+        Button b = (Button)findViewById(R.id.btnAdd);
     }
 
 
--- app/src/main/java/net/skhu/androidmidtest/exam3Adapter.java
+++ app/src/main/java/net/skhu/androidmidtest/exam3Adapter.java
@@ -1,4 +1,49 @@
 package net.skhu.androidmidtest;
 
-public class exam3Adapter extends  {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+
+public class exam3Adapter extends RecyclerView.Adapter<exam3Adapter.ViewHolder>{
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        TextView textView;
+
+        public ViewHolder(View view){
+            super(view);
+            textView = view.findViewById(R.id.textView1);
+        }
+    }
+
+    LayoutInflater layoutInflater;
+    ArrayList<String> arrayList;
+
+    public exam3Adapter(Context context, ArrayList<String> arrayList) {
+        this.layoutInflater = LayoutInflater.from(context);
+        this.arrayList = arrayList;
+    }
+
+    @Override
+    public exam3Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = layoutInflater.inflate(R.layout.title, viewGroup, false);
+        return new ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull exam3Adapter.ViewHolder viewHolder, int index) {
+        viewHolder.textView.setText(arrayList.get(index));
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
 }
